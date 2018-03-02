d4beb4a94d976c3c4a129b80ce77a06c7aa5392f
Alex Ruiz <ruiz.alex0302@gmail.com>
2018-03-02 01:03:36 -0800

    Update choose-your-own-story sample code

--- ChooseStory.java
+++ ChooseStory.java
@@ -1,17 +1,46 @@
 import java.io.*;
+import java.util.*;
 
 class Main {
-  public static void main(String[] args) {
+  //An ArrayList of every prompt that the user can be given.
+  static ArrayList<String> prompts = new ArrayList<String>();
+
+  public static void main(String[] args) throws IOException {
+    //Set up the console
     Console c = System.console();
     if (c == null){
       System.out.println("No console :(");
       System.exit(1);
     }
-    System.out.println("Type 'stop' to exit");
-    while (true){
-      String echo = c.readLine("Ask me a question: ");
-      if (echo.equals("stop")) System.exit(0);
-      System.out.println(answer[(int)(Math.random()*answer.length)]);
+
+    //Use a scanner to scan foreststory.txt and pull out all the prompts
+    Scanner in = null;
+    in = new Scanner(new BufferedReader(new FileReader("foreststory.txt"))).useDelimiter("\n\n");
+    for (int i = 0; in.hasNext(); i++){
+      prompts.add(in.next());
     }
+
+    //Game logic here
+    String answer = prompt(0);
+    if (answer.equals("B")) {
+
+      answer = prompt(2); //Good choice, proceed to answer #3
+      if (answer.equals("A")) end(4); //Good ending, answer #4
+      else if (answer.equals("B")) end(5); //Bad ending, answer #5
+
+      else System.out.println("Invalid input.");
+
+    } else if (answer.equals("A")) {
+
+      end(3); //Bad ending, answer #2
+
+    } else System.out.println("Invalid input.");
+  }
+  static String prompt(int index){
+    System.out.println(prompts.get(index));
+    return System.console().readLine("Your choice (type character): ");
+  }
+  static void end(int index){
+    System.out.println(prompts.get(index));
   }
 }
